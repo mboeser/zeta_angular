a655d18b5c792cfccb499f5d12ae4fefcb66c28f
Scott Bromander <scott@primeacademy.io>
2015-11-09 14:39:26 -0600

    complete

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,25 @@
 var myApp = angular.module('myApp', []);
 
+
+
+myApp.controller("WelcomeController",['$scope', '$http', function($scope, $http){
+    $scope.note = {};
+    $scope.nameArray = [];
+
+    //POST
+    $scope.clickButton = function(kittyFooFoo){
+        $http.post('/people', kittyFooFoo).then(function(response){
+            $scope.getPeople();
+        });
+    };
+
+    //GET
+    $scope.getPeople = function(){
+        $http.get('/people').then(function(response){
+            $scope.nameArray = response.data;
+        });
+    };
+
+    $scope.getPeople();
+}]);
+
--- server/app.js
+++ server/app.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 app.set("port", process.env.PORT || 5000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost/angular_first_project');
+
+var Person = mongoose.model('person', new Schema({name: String}));
+
+app.get("/people", function(req,res){
+    Person.find({}, function(err, data){
+        if(err) console.log(err);
+        res.send(data);
+    });
+});
+
+app.post("/people", function(req,res){
+    var person = new Person({name: req.body.name});
+    person.save(function(err, data){
+        if(err) console.log(err);
+        res.send(data);
+    })
+});
 
 app.get("/*", function(req,res){
     var file = req.params[0] || "views/index.html";
